33c64f2aad219b73aaeb444c00fe96c41b71d5a6
Shinusei <lyshagrishin@gmail.com>
2024-10-08 02:12:38 +0300

    date picker

--- app/src/main/java/com/shinusei/headachemonitor/MainActivity.kt
+++ app/src/main/java/com/shinusei/headachemonitor/MainActivity.kt
@@ -1,29 +1,28 @@
 package com.shinusei.headachemonitor
 
-import android.R
-import android.icu.util.Calendar
-import android.opengl.Visibility
 import android.os.Bundle
-import android.widget.CalendarView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.DateRangePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -31,14 +30,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.shinusei.headachemonitor.ui.theme.HeadacheMonitorTheme
-import java.time.Instant
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
-import kotlin.text.format
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,7 +43,6 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    //Calendar()
                     DiceRollerApp()
                 }
 
@@ -60,40 +54,65 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun DiceRollerApp() {
-    DiceWithButtonAndImage(modifier = Modifier
-        .fillMaxSize()
-        .wrapContentSize(Alignment.Center)
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .wrapContentSize(Alignment.Center)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
 
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun Calendar(){
-    val dateState = rememberDatePickerState()
-    val dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-
-    Column (horizontalAlignment = Alignment.CenterHorizontally){
-        DatePicker(state = dateState)
-        Text("Selected date: " +
-                "${dateState.selectedDateMillis?.let {
-                    val instant = Instant.ofEpochMilli(it)
-                    val date = instant.atZone(ZoneId.systemDefault()).toLocalDate()
-                    dateFormatter.format(date)
-                }}")
-    }
-}
-
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
-    var txt = "123"
+    var showModal by remember { mutableStateOf(false) }
+    var selectedDate by remember { mutableStateOf<Long?>(null) }
 
-    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+    Column(modifier = modifier) {
         Button(
-            onClick = {  },
+            onClick = { showModal = true },
         ) {
-            Text(text = txt, style = MaterialTheme.typography.bodyLarge )
+            Text(text = "Select Date Range", style = MaterialTheme.typography.bodyLarge)
+
+            if (showModal) {
+                DatePickerModal(
+                    onDateSelected = {
+                        selectedDate = it
+                        showModal = false
+                    },
+                    onDismiss = { showModal = false }
+                )
+            }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerModal(
+    onDateSelected: (Long?) -> Unit,
+    onDismiss: () -> Unit
+) {
+    val datePickerState = rememberDatePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = {
+                onDateSelected(datePickerState.selectedDateMillis)
+                onDismiss()
+            }){ Text(text = "Ок")}
+        },
+        dismissButton = {
+            TextButton(onClick = onDismiss) {
+                Text(text = "Отмена")
+            }
+        }
+    ) {
+        DatePicker(state = datePickerState)
+    }
+}
